4d90ad4010fd2a259abc2ffc696c9653d3d12abc
shotaro427 <toshinobu.s111@gmail.com>
2020-01-03 14:47:31 +0900

    トップ画面のサンプルレイアウトを作成

--- lib/widgets/top_image_list/top_image_list.dart
+++ lib/widgets/top_image_list/top_image_list.dart
@@ -9,14 +9,36 @@ class TopImagesPage extends StatelessWidget {
 
   TopImagesPage(this.roomInfo);
 
+  final List<IconData> icons = [
+    Icons.access_alarm,
+    Icons.arrow_back_ios,
+    Icons.account_balance_wallet,
+    Icons.center_focus_weak,
+    Icons.blur_on,
+    Icons.dashboard,
+    Icons.phone,
+    Icons.tap_and_play,
+  ];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: const Text("一覧"),),
-      body: Center(
-        child: Text(roomInfo["name"].toString()),
-      ),
+      appBar: AppBar(title: Text(roomInfo["name"].toString()),),
+      body: GridView.builder(
+        itemCount: icons.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        ),
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+            border: Border.all(color: Colors.black45),
+            ),
+            child: Icon(icons[index], size: 50),
+          );
+        },
+      )
     );
   }
 }
