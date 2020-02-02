867985581da388eaa611edb35027a7bb48943a52
shotaro427 <toshinobu.s111@gmail.com>
2020-02-02 10:25:56 +0900

    トップ画像を読み込むときにアニメーション付きで表示されるように変更

--- lib/models/image_upload_bloc.dart
+++ lib/models/image_upload_bloc.dart
@@ -1,10 +1,8 @@
 
-
 import 'dart:async';
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:filesize/filesize.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:image_picker/image_picker.dart';
--- lib/widgets/top_image_list/top_image_list.dart
+++ lib/widgets/top_image_list/top_image_list.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:typed_data';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -63,23 +65,10 @@ class _ImagesWidget extends StatelessWidget {
                 onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => ImageDetailPage(snapshot.data[index]))),
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(10),
-                  child: Image(
+                  child: FadeInImage.memoryNetwork(
                     fit: BoxFit.cover,
-                    image: (snapshot.hasData)
-                      ? NetworkImage(snapshot.data[index].data['url'])
-                      : Card(color: Colors.orange,),
-                    loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent loadingProgress) {
-                      if (loadingProgress == null) {
-                        return child;
-                      }
-                      return Center(
-                        child: CircularProgressIndicator(
-                          value: loadingProgress.expectedTotalBytes != null
-                          ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes
-                              : null,
-                        ),
-                      );
-                    },
+                    placeholder: kTransparentImage,
+                    image: snapshot.data[index].data['url'],
                   ),
                 ),
               );
@@ -90,3 +79,70 @@ class _ImagesWidget extends StatelessWidget {
     );
   }
 }
+
+final Uint8List kTransparentImage = new Uint8List.fromList(<int>[
+  0x89,
+  0x50,
+  0x4E,
+  0x47,
+  0x0D,
+  0x0A,
+  0x1A,
+  0x0A,
+  0x00,
+  0x00,
+  0x00,
+  0x0D,
+  0x49,
+  0x48,
+  0x44,
+  0x52,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+  0x08,
+  0x06,
+  0x00,
+  0x00,
+  0x00,
+  0x1F,
+  0x15,
+  0xC4,
+  0x89,
+  0x00,
+  0x00,
+  0x00,
+  0x0A,
+  0x49,
+  0x44,
+  0x41,
+  0x54,
+  0x78,
+  0x9C,
+  0x63,
+  0x00,
+  0x01,
+  0x00,
+  0x00,
+  0x05,
+  0x00,
+  0x01,
+  0x0D,
+  0x0A,
+  0x2D,
+  0xB4,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x49,
+  0x45,
+  0x4E,
+  0x44,
+  0xAE,
+]);
