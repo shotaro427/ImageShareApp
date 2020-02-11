44a6ad29c8b4397f88203c00a8edd6cc7d521d2c
shotaro427 <toshinobu.s111@gmail.com>
2020-02-11 11:31:59 +0900

    招待する処理を追加

--- lib/widgets/room_settings/add_member_page.dart
+++ lib/widgets/room_settings/add_member_page.dart
@@ -5,20 +5,28 @@ import 'package:flutter/material.dart';
 
 class AddMemberPage extends StatelessWidget {
 
-  final TextEditingController _emailController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("メンバー招待"),),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: TextFormField(
-            controller: _emailController,
-            decoration: const InputDecoration(
+      body: _AddMemberLayout()
+    );
+  }
+}
+
+class _AddMemberLayout extends StatelessWidget {
+
+  final TextEditingController _emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: TextFormField(
+          controller: _emailController,
+          decoration: const InputDecoration(
               hintText: "招待したい人のメールアドレス"
-            ),
           ),
         ),
       ),
