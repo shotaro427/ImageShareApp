4a9d639f1d03d81b02b314cc015ba80510b87d8d
shotaro427 <toshinobu.s111@gmail.com>
2020-02-16 14:29:46 +0900

    名前を編集するページのレイアウトを作成

---
+++ lib/widgets/room_settings/editing_profile_page.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class EditingProfilePage extends StatelessWidget {
+
+  final TextEditingController _nickNameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: const Text('編集'),),
+      body: Center(
+        child: TextFormField(
+          controller: _nickNameController,
+          decoration: const InputDecoration(
+            border: const UnderlineInputBorder(),
+            labelText: 'ニックネーム',
+          ),
+          obscureText: true,
+        ),
+      ),
+    );
+  }
+}
