fcb516599d8e3a0ed6c06d7e7e2268432cc0b216
shotaro427 <toshinobu.s111@gmail.com>
2020-02-16 15:17:34 +0900

    レイアウトの修正

--- lib/widgets/room_settings/editing_profile_page.dart
+++ lib/widgets/room_settings/editing_profile_page.dart
@@ -4,17 +4,31 @@ import 'package:flutter/material.dart';
 
 class EditingProfilePage extends StatelessWidget {
 
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('編集'),),
+        body: _EditingProfileWidget()
+      ),
+    );
+  }
+}
+
+class _EditingProfileWidget extends StatelessWidget {
+
   final TextEditingController _nickNameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(title: const Text('編集'),),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: TextFormField(
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextFormField(
             controller: _nickNameController,
             decoration: const InputDecoration(
               border: const UnderlineInputBorder(),
@@ -22,7 +36,19 @@ class EditingProfilePage extends StatelessWidget {
             ),
             obscureText: true,
           ),
-        ),
+          const SizedBox(height: 30,),
+          FlatButton(
+            child: const Text('編集'),
+            onPressed: () {
+
+            },
+            color: Colors.white,
+            shape: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            ),
+            highlightColor: Colors.blue,
+          )
+        ],
       ),
     );
   }
