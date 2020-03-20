694ef457ef8807499e81722965314503bc6e3257
shotaro427 <toshinobu.s111@gmail.com>
2020-03-20 15:12:46 +0900

    EmailでのサインインのRepositoryクラスを作成&NotifierクラスでそのクラスをDI

--- lib/models/sign_in_with_email_model.dart
+++ lib/models/sign_in_with_email_model.dart
@@ -1,14 +1,14 @@
 
-import 'dart:js';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:developer';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:image_share_app/Entities/user_entity/user_entity.dart';
 import 'package:image_share_app/repositories/room_list_repository.dart';
+import 'package:image_share_app/repositories/sign_in_repositories/sign_in_with_email_repository.dart';
 import 'package:image_share_app/widgets/room_list/room_list.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:state_notifier/state_notifier.dart';
 part 'sign_in_with_email_model.freezed.dart';
 
@@ -16,54 +16,40 @@ part 'sign_in_with_email_model.freezed.dart';
 abstract class SignInWithEmailState with _$SignInWithEmailState {
   const factory SignInWithEmailState() = _SignInWithEmailState;
   const factory SignInWithEmailState.loading() = Loading;
-  const factory SignInWithEmailState.success({@Default(false) bool isCompleted}) = Success;
+  const factory SignInWithEmailState.success({@Default(UserEntity(email: '', uid: '')) UserEntity user}) = Success;
   const factory SignInWithEmailState.error({@Default('') String message}) = ErrorDetails;
 }
 
 class SignInWithEmailStateNotifier extends StateNotifier<SignInWithEmailState> {
 
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final SignInWithEmailRepository _repository;
 
-  SignInWithEmailStateNotifier(): super(const SignInWithEmailState());
+  SignInWithEmailStateNotifier(this._repository): super(const SignInWithEmailState());
 
   /// メールアドレスとパスワードを使ってサインインする
-  Future<FirebaseUser> signIn(String email, String password) async {
-    final FirebaseUser user = (await _auth.signInWithEmailAndPassword(
-        email: email, password: password)).user;
+  Future<void> logInWithEmail(String email, String password) async {
+    state = const SignInWithEmailState.loading();
 
-    // ローカルにuidを更新
-    final SharedPreferences _prefs = await SharedPreferences.getInstance();
-    await _prefs.setString('uid', user.uid);
-    await _prefs.setString('email', user.email);
-
-    await Firestore.instance.collection('users').where('uid', isEqualTo: user.uid)
-        .getDocuments()
-        .then((docs) {
-      // ユーザーが存在しなかった場合追加する
-      if (docs.documents.length == 0) {
-        Firestore.instance.collection('users').document('${user.uid}').setData({
-          'email': user.email,
-          'uid': user.uid
-        });
-      }
-    }).catchError((e) => debugPrint(e.toString()));
-
-    debugPrint("User id is ${user.uid}");
-    return user;
+    try {
+      UserEntity _user = await _repository.loginWithEmailAndPassword(email, password);
+      state = SignInWithEmailState.success(user: _user);
+      return;
+    } catch(e) {
+      state = SignInWithEmailState.error(message: e.toString());
+      log(e.toString());
+      return;
+    }
   }
 
-  /// ローカルにユーザー情報を保存する関数
-
   /// 次のページに遷移させる
-  void transitionNextPage(FirebaseUser user, BuildContext context) {
+  void transitionRoomListPage(UserEntity user, BuildContext context) {
     if (user == null) return;
 
     Navigator.of(context).pushAndRemoveUntil(
         MaterialPageRoute(
             builder: (BuildContext context) => RoomListPage(RoomListRepository())
         ),
-        ModalRoute.withName("/home")
+        (_) => false
     );
   }
 }
-
---
+++ lib/repositories/sign_in_repositories/sign_in_with_email_repository.dart
@@ -0,0 +1,51 @@
+
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_share_app/Entities/user_entity/user_entity.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SignInWithEmailRepository {
+
+  /// ログインする
+  Future<UserEntity> loginWithEmailAndPassword(String email, String password) async {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+
+    try {
+      // ログイン
+      final FirebaseUser user = (await _auth.signInWithEmailAndPassword(
+          email: email, password: password)).user;
+
+      final UserEntity _userEntity = UserEntity(
+          email: user.email, uid: user.uid);
+
+      _saveUserInfo(_userEntity);
+
+      return _userEntity;
+    } catch(e) {
+      log(e.toString());
+      return null;
+    }
+  }
+
+  /// ユーザー情報を保存する関数
+  Future<void> _saveUserInfo(UserEntity user) async {
+    // ローカルにuidを更新
+    final SharedPreferences _prefs = await SharedPreferences.getInstance();
+    await _prefs.setString('uid', user.uid);
+    await _prefs.setString('email', user.email);
+
+    await Firestore.instance.collection('users').where('uid', isEqualTo: user.uid)
+        .getDocuments()
+        .then((docs) {
+      // ユーザーが存在しなかった場合追加する
+      if (docs.documents.length == 0) {
+        Firestore.instance.collection('users').document('${user.uid}').setData({
+          'email': user.email,
+          'uid': user.uid
+        });
+      }
+    }).catchError((e) => log(e.toString()));
+  }
+}
