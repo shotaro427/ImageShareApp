7bfa7dd65b44e634e174be3d53bf75f693fc10c6
shotaro427 <toshinobu.s111@gmail.com>
2020-03-26 15:27:02 +0900

    不要なimportを削除

--- lib/widgets/top_image_list/top_image_list.dart
+++ lib/widgets/top_image_list/top_image_list.dart
@@ -1,6 +1,4 @@
 
-import 'dart:developer';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
