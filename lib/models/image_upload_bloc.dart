1fe64bdc088e02768743f4a4fea5f3bf1cb96c67
shotaro427 <toshinobu.s111@gmail.com>
2020-03-27 16:51:10 +0900

    reposirotyクラスを別ファイルに移動

--- lib/models/image_upload_bloc.dart
+++ lib/models/image_upload_bloc.dart
@@ -3,12 +3,7 @@ import 'dart:async';
 import 'dart:developer';
 import 'dart:io';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter_native_image/flutter_native_image.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:image_share_app/widgets/commont_widgets/common_loading_widget.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:image_share_app/repositories/image_list_repositories/image_upload_repository.dart';
 import 'package:flutter/foundation.dart';
 import 'package:state_notifier/state_notifier.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -56,76 +51,5 @@ class ImageUploadStateNotifier extends StateNotifier<ImageUploadState> {
       log(e.toString());
       state = ImageUploadState.error(message: e.toString(), file: File('images/image_placeholder_500_300.png'));
     }
-  }
-}
-
-class ImageUploadRepository {
-
-  /// ユーザーのローカルから画像を選択させて、取得する
-  Future<File> getImageInGallery() async {
-
-    return await ImagePicker.pickImage(source: ImageSource.gallery);
-  }
-
-  /// FireStorageに画像をアップロードする
-  Future uploadImageToFireStorage(File file, String roomId, String timestamp) async {
-
-    final ImageProperties _properties = await FlutterNativeImage.getImageProperties(file.path);
-    File compressedFile = await FlutterNativeImage.compressImage(file.path, targetHeight: 200, targetWidth: (_properties.width / _properties.height * 200).round());
-
-    log(roomId);
-
-    final StorageReference _ref = FirebaseStorage.instance
-        .ref()
-        .child("roomImages/${roomId}/${timestamp}");
-
-    final StorageReference _thumbnailsRef = FirebaseStorage.instance
-        .ref()
-        .child('roomImages/${roomId}/thumbnails/${timestamp}_200x200');
-
-    await _ref.putFile(
-        file,
-        StorageMetadata(
-          contentType: "image/jpeg",
-        )
-    );
-
-    await _thumbnailsRef.putFile(
-      compressedFile,
-      StorageMetadata(
-        contentType: "image/jpeg",
-      )
-    );
-  }
-
-  /// firestoreに投稿情報を保存する
-  Future postImageWithTitle(
-      String roomId,
-      String timestamp,
-      {
-        String title,
-        String memoText
-      }) async {
-
-    String _title = "名無し";
-    String _memoText = "";
-    
-    if (title != null && title.isNotEmpty) { _title = title; }
-    if (memoText != null) { _memoText = memoText; }
-
-    final SharedPreferences _prefs = await SharedPreferences.getInstance();
-    final _uid = _prefs.getString('uid');
-
-    final _ref = await Firestore.instance.collection("rooms/${roomId}/images").add({
-      'title': _title,
-      'memo': _memoText,
-      'created_at': timestamp.toString(),
-      'updated_at': timestamp.toString(),
-      'created_user_uid': _uid
-    });
-
-    await _ref.updateData({
-      'image_id': _ref.documentID
-    });
   }
 }
