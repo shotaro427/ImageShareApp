f15bd229ebb40619619108ee08636de1719137d4
shotaro427 <toshinobu.s111@gmail.com>
2020-03-27 16:04:56 +0900

    投稿画面を改修

--- lib/widgets/top_image_list/image_upload_page.dart
+++ lib/widgets/top_image_list/image_upload_page.dart
@@ -1,8 +1,8 @@
 
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import "package:image_share_app/models/image_upload_bloc.dart";
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:image_share_app/widgets/commont_widgets/common_loading_widget.dart';
 import 'package:provider/provider.dart';
 
 
@@ -14,22 +14,39 @@ class ImageUploadPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Provider<ImageUploadBloc>(
-      create: (context) => ImageUploadBloc(ImageUploadRepository()),
-      dispose: (_, bloc) => bloc.dispose(),
+    return StateNotifierProvider<ImageUploadStateNotifier, ImageUploadState>(
+      create: (_) => ImageUploadStateNotifier(ImageUploadRepository()),
       child: Stack(
-        children: <Widget>[
-          Scaffold(
-            appBar: AppBar(
-              title: const Text("投稿"),
-              elevation: 0,
+          children: <Widget>[
+            Scaffold(
+              appBar: AppBar(
+                title: const Text("投稿"),
+                elevation: 0,
+              ),
+              body: _LayoutUploadImagePage(roomId),
             ),
-            body: _LayoutUploadImagePage(roomId),
-          ),
-          CommonLoadingWidget<ImageUploadBloc>(dialogTitle: "画像のアップロード",)
-        ]
+            // TODO: ローディングのWidgetを追加
+
+          ]
       ),
     );
+
+//    return Provider<ImageUploadBloc>(
+//      create: (context) => ImageUploadBloc(ImageUploadRepository()),
+//      dispose: (_, bloc) => bloc.dispose(),
+//      child: Stack(
+//        children: <Widget>[
+//          Scaffold(
+//            appBar: AppBar(
+//              title: const Text("投稿"),
+//              elevation: 0,
+//            ),
+//            body: _LayoutUploadImagePage(roomId),
+//          ),
+//          CommonLoadingWidget<ImageUploadBloc>(dialogTitle: "画像のアップロード",)
+//        ]
+//      ),
+//    );
   }
 }
 
@@ -45,13 +62,12 @@ class _LayoutUploadImagePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var bloc = Provider.of<ImageUploadBloc>(context, listen: false);
     return GestureDetector(
       onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
       child: SingleChildScrollView(
-        child: StreamBuilder(
-          stream: bloc.value,
-          builder: (context, snapshot) {
+        child: StateNotifierBuilder<ImageUploadState>(
+          stateNotifier: context.read<ImageUploadStateNotifier>(),
+          builder: (context, state, _) {
             return Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
@@ -85,41 +101,49 @@ class _LayoutUploadImagePage extends StatelessWidget {
                       ),
                       // 投稿ボタン
                       IconButton(
-                        icon: Icon(
-                          Icons.send,
-                          color: (!snapshot.hasData) ? Colors.grey : Colors.white,
-                          size: 30,
-                        ),
-                        onPressed: (!snapshot.hasData)
-                            ? null
-                            : () {
-                          // 画像をアップロードする処理
-                          if (snapshot.hasData) {
-                            var bloc = Provider.of<ImageUploadBloc>(context, listen: false);
-                            bloc.uploadImage(snapshot.data, roomId,
-                                title: titleController.text,
-                                memoText: memoController.text);
+                          icon: Icon(
+                            Icons.send,
+                            color: state.maybeWhen(
+                                () => Colors.grey,
+                                success: (_) => Colors.white,
+                                orElse: () => Colors.grey
+                            ),
+                            size: 30,
+                          ),
+                          onPressed: () async {
+                            state.maybeWhen(
+                                () => null,
+                                success: (imageFile) async => await context.read<ImageUploadStateNotifier>().uploadImage(imageFile, roomId, title: titleController.text, memo: memoController.text),
+                                orElse: () => null
+                            );
                           }
-                        }
                       ),
                     ],
                   ),
                 ),
                 // アップロードする画像を表示するWidget
                 GestureDetector(
-                  onTap: () => Provider.of<ImageUploadBloc>(context, listen: false).pickUpImage(),
+                  onTap: () => context.read<ImageUploadStateNotifier>().pickUpImage(),
                   child: Container(
                     height: 250,
-                    child: (snapshot.hasData)
-                        ? Image(
-                      image: FileImage(snapshot.data),
-                      fit: BoxFit.scaleDown,
-                    )
-                        : const Image(
-                      image: const AssetImage("images/image_placeholder_500_300.png"),
-                      fit: BoxFit.cover,
-                    ),
                     padding: const EdgeInsets.all(5),
+                    child: state.maybeWhen(
+                        // デフォルト時
+                        () => const Image(
+                          image: const AssetImage("images/image_placeholder_500_300.png"),
+                          fit: BoxFit.cover,
+                        ),
+                        //　画像選択が成功したとき
+                        success: (imageFile) => Image(
+                          image: FileImage(imageFile),
+                          fit: BoxFit.scaleDown,
+                        ),
+                        // そのほかの時
+                        orElse: () => const Image(
+                          image: const AssetImage("images/image_placeholder_500_300.png"),
+                          fit: BoxFit.cover,
+                        ),
+                    ),
                   ),
                 ),
                 // タグ追加欄
@@ -173,8 +197,134 @@ class _LayoutUploadImagePage extends StatelessWidget {
                 ),
               ],
             );
-          }
-        )
+          },
+        ),
+//        child: StreamBuilder(
+//          stream: bloc.value,
+//          builder: (context, snapshot) {
+//            return Column(
+//              mainAxisAlignment: MainAxisAlignment.start,
+//              children: <Widget>[
+//                // タイトル入力欄
+//                Container(
+//                  padding: const EdgeInsets.symmetric(horizontal: 15),
+//                  decoration: BoxDecoration(
+//                    color: Theme
+//                        .of(context)
+//                        .primaryColor,
+//                  ),
+//                  child: Row(
+//                    children: <Widget>[
+//                      // タイトル入力欄
+//                      Flexible(
+//                        child: TextFormField(
+//                          style: const TextStyle(color: Colors.white),
+//                          controller: titleController,
+//                          autofocus: true,
+//                          cursorColor: Theme
+//                              .of(context)
+//                              .primaryTextTheme
+//                              .title
+//                              .color,
+//                          decoration: const InputDecoration(
+//                            border: InputBorder.none,
+//                            hintText: 'タイトル',
+//                            hintStyle: const TextStyle(color: Colors.white),
+//                          ),
+//                        ),
+//                      ),
+//                      // 投稿ボタン
+//                      IconButton(
+//                        icon: Icon(
+//                          Icons.send,
+//                          color: (!snapshot.hasData) ? Colors.grey : Colors.white,
+//                          size: 30,
+//                        ),
+//                        onPressed: (!snapshot.hasData)
+//                            ? null
+//                            : () {
+//                          // 画像をアップロードする処理
+//                          if (snapshot.hasData) {
+//                            var bloc = Provider.of<ImageUploadBloc>(context, listen: false);
+//                            bloc.uploadImage(snapshot.data, roomId,
+//                                title: titleController.text,
+//                                memoText: memoController.text);
+//                          }
+//                        }
+//                      ),
+//                    ],
+//                  ),
+//                ),
+//                // アップロードする画像を表示するWidget
+//                GestureDetector(
+//                  onTap: () => Provider.of<ImageUploadBloc>(context, listen: false).pickUpImage(),
+//                  child: Container(
+//                    height: 250,
+//                    child: (snapshot.hasData)
+//                        ? Image(
+//                      image: FileImage(snapshot.data),
+//                      fit: BoxFit.scaleDown,
+//                    )
+//                        : const Image(
+//                      image: const AssetImage("images/image_placeholder_500_300.png"),
+//                      fit: BoxFit.cover,
+//                    ),
+//                    padding: const EdgeInsets.all(5),
+//                  ),
+//                ),
+//                // タグ追加欄
+//                Row(
+//                  children: <Widget>[
+//                    Padding(
+//                      padding: const EdgeInsets.only(left: 20, right: 15, top: 10, bottom: 10),
+//                      child: Icon(
+//                        Icons.local_offer,
+//                        color: Colors.grey,
+//                        size: 22,
+//                      ),
+//                    ),
+//                    const Text('タグ', style: TextStyle(color: Colors.grey),)
+//                  ],
+//                ),
+//                Padding(
+//                  padding: const EdgeInsets.symmetric(horizontal: 20),
+//                  child: Divider(
+//                    color: Theme.of(context).colorScheme.onSurface,
+//                  ),
+//                ),
+//                // メモ入力画面
+//                Row(
+//                  crossAxisAlignment: CrossAxisAlignment.start,
+//                  children: <Widget>[
+//                    Padding(
+//                      padding: const EdgeInsets.only(left: 20, right: 15, top: 13),
+//                      child: Icon(
+//                        Icons.note,
+//                        color: Colors.grey,
+//                        size: 22,
+//                      ),
+//                    ),
+//                    Flexible(
+//                      child: TextFormField(
+//                        controller: memoController,
+//                        decoration: const InputDecoration(
+//                          border: InputBorder.none,
+//                          hintText: 'メモ',
+//                          hintStyle: TextStyle(
+//                              color: Colors.grey,
+//                              fontSize: 14
+//                          ),
+//                        ),
+//                        maxLines: null,
+//                        keyboardType: TextInputType.multiline,
+//                      ),
+//                    ),
+//                  ],
+//                ),
+//              ],
+//            );
+//          }
+//        )
       ),
     );
   }
