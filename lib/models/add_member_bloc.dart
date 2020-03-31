47bcd88c7587d3c67ce413605399f42d2333db14
shotaro427 <toshinobu.s111@gmail.com>
2020-03-31 17:24:45 +0900

    不要な箇所を削除

--- lib/models/add_member_bloc.dart
+++ lib/models/add_member_bloc.dart
@@ -38,23 +38,3 @@ class AddMemberStateNotifier extends StateNotifier<AddMemberState> {
     }
   }
 }
-
-class AddMemberBloc {
-
-  inviteUser(String email, DocumentReference roomRef) async {
-    
-    final Query _query = Firestore.instance
-        .collection("users")
-        .where("email", isEqualTo: email);
-
-    DocumentReference _userRef;
-
-    await _query.getDocuments().then((docs) {
-        _userRef = docs.documents.first.reference;
-    }).catchError((e) => debugPrint(e.toString()));
-    
-    await _userRef.collection('waitingRooms').add({
-      'room': roomRef
-    });
-  }
-}
