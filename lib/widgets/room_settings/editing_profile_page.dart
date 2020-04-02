7a1511e08c1aa0e7e11d8914dc64fc0ae9e1107a
shotaro427 <toshinobu.s111@gmail.com>
2020-04-02 10:43:40 +0900

    不要な記述を削除

--- lib/widgets/room_settings/editing_profile_page.dart
+++ lib/widgets/room_settings/editing_profile_page.dart
@@ -25,23 +25,6 @@ class EditingProfilePage extends StatelessWidget {
         ],
       ),
     );
-//    // TODO: implement build
-//    return Provider<EditingProfileBloc>(
-//      create: (_) => EditingProfileBloc(EditingProfileRepository()),
-//      dispose: (_, bloc) => bloc.dispose(),
-//      child: Stack(
-//        children: <Widget>[
-//          GestureDetector(
-//            onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
-//            child: Scaffold(
-//              appBar: AppBar(title: const Text('編集'),),
-//              body: _EditingProfileWidget()
-//            ),
-//          ),
-//          CommonLoadingWidget<EditingProfileBloc>(dialogTitle: '名前の変更',)
-//        ],
-//      ),
-//    );
   }
 }
 
@@ -89,40 +72,9 @@ class _EditingProfileWidget extends StatelessWidget {
         );
       },
     );
-
-//    final EditingProfileBloc _bloc = Provider.of<EditingProfileBloc>(context, listen: false);
-//    return Padding(
-//      padding: const EdgeInsets.all(20),
-//      child: Column(
-//        mainAxisAlignment: MainAxisAlignment.center,
-//        children: <Widget>[
-//          TextFormField(
-//            controller: _nickNameController,
-//            decoration: const InputDecoration(
-//              border: const UnderlineInputBorder(),
-//              labelText: 'ニックネーム',
-//            ),
-//          ),
-//          const SizedBox(height: 30,),
-//          FlatButton(
-//            child: const Text('編集'),
-//            onPressed: () {
-//              final String _name = _nickNameController.text;
-//              if (_name != null && _name.isNotEmpty) {
-//                _bloc.editingName(_name);
-//              }
-//            },
-//            color: Colors.white,
-//            shape: const OutlineInputBorder(
-//              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-//            ),
-//            highlightColor: Colors.blue,
-//          )
-//        ],
-//      ),
-//    );
   }
 
+  /// 編集ボタンを押した後の挙動の処理
   void showResultDialog(BuildContext context, EditingProfileState state) {
     state.maybeWhen(
             () => null,
