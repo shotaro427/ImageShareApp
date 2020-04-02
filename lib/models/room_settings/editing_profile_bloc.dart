319cfd6a14356aac9cfa29eb5171b432772968a0
shotaro427 <toshinobu.s111@gmail.com>
2020-04-02 10:48:11 +0900

    repositoryクラスを移動

--- lib/models/room_settings/editing_profile_bloc.dart
+++ lib/models/room_settings/editing_profile_bloc.dart
@@ -1,11 +1,10 @@
 
 import 'dart:developer';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:image_share_app/repositories/room_settings_repositories/editing_profile_repository.dart';
 import 'package:state_notifier/state_notifier.dart';
 part 'editing_profile_bloc.freezed.dart';
 
@@ -33,31 +32,6 @@ class EditingProfileStateNotifier extends StateNotifier<EditingProfileState> {
     } catch(e) {
       log(e.toString());
       state = EditingProfileState.error(message: e.toString());
-    }
-  }
-}
-
-class EditingProfileRepository {
-
-  /// ローカルストレージに名前を保存する関数
-  void setNickNameIntoLocalStorage(String name) async {
-
-    final SharedPreferences _prefs = await SharedPreferences.getInstance();
-    await _prefs.setString('name', name);
-  }
-
-  /// FireStoreに名前を保存する関数
-  void setNickNameIntoFireStore(String name) async {
-
-    final SharedPreferences _prefs = await SharedPreferences.getInstance();
-    final _uid = _prefs.getString('uid');
-
-    final QuerySnapshot _querySnapshot = await Firestore.instance.collection('users').where('uid', isEqualTo: _uid).getDocuments();
-
-    if (_querySnapshot.documents.first.reference != null) {
-      await  _querySnapshot.documents.first.reference.updateData({
-        'name': name,
-      });
     }
   }
 }
