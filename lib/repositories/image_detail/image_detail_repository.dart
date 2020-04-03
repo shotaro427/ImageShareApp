1688fa5618fde25e05c8f1001048fbb89a71d9f7
shotaro427 <toshinobu.s111@gmail.com>
2020-04-03 10:33:08 +0900

    投稿をFireStoreとCloud Storageから削除する処理を追加

--- lib/repositories/image_detail/image_detail_repository.dart
+++ lib/repositories/image_detail/image_detail_repository.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_share_app/Entities/image_entity/image_entity.dart';
 import 'package:image_share_app/Entities/room_entity/room_info_entity.dart';
 
@@ -18,5 +19,19 @@ class ImageDetailRepository {
     final document = await Firestore.instance.document('rooms/${roomInfoEntity.roomId}/images/${imageEntity.image_id}').get();
 
     return ImageEntity.fromJson(document.data);
+  }
+  
+  /// 投稿を削除する
+  Future<void> deleteImage(ImageEntity imageEntity, RoomInfoEntity roomInfoEntity) async {
+    
+    // FireStoreから投稿を削除
+    await Firestore.instance.document('rooms/${roomInfoEntity.roomId}/images/${imageEntity.image_id}').delete();
+    
+    // Cloud Storageから画像を削除
+    await FirebaseStorage.instance.ref().child('roomImages/${roomInfoEntity.roomId}/${imageEntity.created_at}').delete();
+
+    // Cloud Storageからサムネイル画像を削除
+    await FirebaseStorage.instance.ref().child('roomImages/${roomInfoEntity.roomId}/thumbnails/${imageEntity.created_at}_200x200').delete();
+
   }
 }
