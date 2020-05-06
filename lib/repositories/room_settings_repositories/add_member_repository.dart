9cbbc55f3f153f93f1a627ee16ee8e666a7247dc
shotaro427 <toshinobu.s111@gmail.com>
2020-05-06 17:19:55 +0900

    すでに招待しているユーザーなどを再度招待できないようにした

--- lib/repositories/room_settings_repositories/add_member_repository.dart
+++ lib/repositories/room_settings_repositories/add_member_repository.dart
@@ -14,12 +14,50 @@ class AddMemberRepository {
 
     QuerySnapshot _querySnapshot = await _query.getDocuments();
 
-    final _userRef = _querySnapshot.documents.first.reference;
+    final DocumentSnapshot _user = _querySnapshot.documents.first;
+    final DocumentReference _userRef = _user.reference;
 
     final DocumentReference _roomRef = Firestore.instance.document('rooms/${roomInfoEntity.roomId}');
 
-    await _userRef.collection('waitingRooms').add({
+    // すでに招待されていたり、メンバーだったりしないかをチェックする
+    if (await _validateSameUser(_user, roomInfoEntity)) throw Exception('This user is already invited');
+
+    // ユーザーのwaitingRoomsに追加
+    final _addRoom =  _userRef.collection('waitingRooms').add({
       'room': _roomRef
     });
+
+    // グループのpendingに追加
+    final _addUser = _roomRef.collection('pending').add({
+      'user': _userRef,
+    });
+
+    await Future.wait([_addRoom, _addUser]);
+  }
+
+  /// 既に招待されていたり、グループに入っていたりするユーザーを弾く
+  Future<bool> _validateSameUser(DocumentSnapshot user, RoomInfoEntity roomInfoEntity) async {
+    final String _targetUid = user.data['uid'];
+
+    final QuerySnapshot _pendings = await Firestore.instance.document('rooms/${roomInfoEntity.roomId}').collection('pending').getDocuments();
+    final QuerySnapshot _members = await Firestore.instance.document('rooms/${roomInfoEntity.roomId}').collection('participants').getDocuments();
+
+    // 招待者をチェック
+    for (final ele in _pendings.documents) {
+      final _user = await (ele.data['user'] as DocumentReference).get();
+      if (_user.data['uid'].toString() == _targetUid) {
+        return true;
+      }
+    }
+
+    // 参加者をチェック
+    for (final ele in _members.documents) {
+      final _user = await (ele.data['user'] as DocumentReference).get();
+      if (_user.data['uid'].toString() == _targetUid) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
