9051972edc966e6cbba2b69e88b87daf01166d9f
shotaro427 <toshinobu.s111@gmail.com>
2020-07-04 13:06:19 +0900

    タグ選択画面を作成

---
+++ lib/widgets/top_image_list/select_tag_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class SelectTagPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
