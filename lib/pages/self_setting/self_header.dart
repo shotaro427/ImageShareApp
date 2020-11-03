eb4fb46b016ed213ac4b24eb4fe0e43fefb81cea
shotaro427 <toshinobu.s111@gmail.com>
2020-11-03 09:41:59 +0900

    modify self_header

--- lib/pages/self_setting/self_header.dart
+++ lib/pages/self_setting/self_header.dart
@@ -7,65 +7,31 @@ class SelfHeader extends StatelessWidget {
     final iconSize = headerHeight * 0.8;
 
     return Container(
-      margin: const EdgeInsets.only(bottom: 24),
-      child: Column(
+      margin: const EdgeInsets.only(bottom: 24, top: 24),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          Stack(
-            children: [
-              SizedBox(
-                height: headerHeight,
-                child: Stack(
-                  children: [
-                    Container(
-                      decoration: const BoxDecoration(
-                        color: Colors.blue,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Align(
-                        alignment: Alignment.bottomRight,
-                        child: Image.asset(
-                          'images/take_image.png',
-                          scale: 1.1,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Container(
-                    margin:
-                        EdgeInsets.only(top: headerHeight / 2 * 1.2, left: 8),
-                    child: SizedBox(
-                      height: iconSize,
-                      width: iconSize,
-                      child: Stack(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.red,
-                              borderRadius: BorderRadius.circular(iconSize / 2),
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.bottomRight,
-                            child: Image.asset(
-                              'images/take_image.png',
-                              scale: 1.1,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+          SizedBox(
+            height: iconSize,
+            width: iconSize,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(iconSize / 2),
                   ),
-                ],
-              ),
-            ],
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Image.asset(
+                    'images/take_image.png',
+                    scale: 1.1,
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
