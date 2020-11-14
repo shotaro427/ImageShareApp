a39bed6193adb826f4ed926d300bf3d589d16bb5
shotaro427 <toshinobu.s111@gmail.com>
2020-11-14 13:32:11 +0900

    add prop to state

--- lib/model/controllers/post_detail_controller/post_detail_state.dart
+++ lib/model/controllers/post_detail_controller/post_detail_state.dart
@@ -1,4 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:image_share_app/model/entities/image.entity.dart';
+import 'package:image_share_app/model/entities/pdf.entity.dart';
 part 'post_detail_state.freezed.dart';
 part 'post_detail_state.g.dart';
 
@@ -9,6 +11,8 @@ abstract class PostDetailState with _$PostDetailState {
     @Default(false) bool isLoading,
     @Default(0) int currentIndex,
     @Default(0) int imageIndex,
+    @Default([]) List<ImageState> images,
+    @Default([]) List<PdfState> pdfs,
   }) = _PostDetailState;
 
   factory PostDetailState.fromJson(Map<String, dynamic> json) =>
--- lib/model/controllers/post_detail_controller/post_detail_state.g.dart
+++ lib/model/controllers/post_detail_controller/post_detail_state.g.dart
@@ -12,6 +12,17 @@ _$_PostDetailState _$_$_PostDetailStateFromJson(Map<String, dynamic> json) {
     isLoading: json['isLoading'] as bool ?? false,
     currentIndex: json['currentIndex'] as int ?? 0,
     imageIndex: json['imageIndex'] as int ?? 0,
+    images: (json['images'] as List)
+            ?.map((e) => e == null
+                ? null
+                : ImageState.fromJson(e as Map<String, dynamic>))
+            ?.toList() ??
+        [],
+    pdfs: (json['pdfs'] as List)
+            ?.map((e) =>
+                e == null ? null : PdfState.fromJson(e as Map<String, dynamic>))
+            ?.toList() ??
+        [],
   );
 }
 
@@ -21,4 +32,6 @@ Map<String, dynamic> _$_$_PostDetailStateToJson(_$_PostDetailState instance) =>
       'isLoading': instance.isLoading,
       'currentIndex': instance.currentIndex,
       'imageIndex': instance.imageIndex,
+      'images': instance.images,
+      'pdfs': instance.pdfs,
     };
